cd3c9f2be43064e3dd2a2b0ab6f4a064349772f1
Nikodem B <nik13go@gmail.com>
2024-04-22 18:00:27 +0200

    update

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-
+let jumping = 0
 
 function losowanie() {
     const random = -((Math.random() * 400) +200);
     hole.style.top = random + 'px';
 }
-hole.addEventListener('animationiteration',losowanie);
+hole.addEventListener('animationiteration', losowanie);
+setInterval(function () {
+    const characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if (jumping === 0) {
+        character.style.top = (characterTop + 1) +"px;"
+    };
+}, 10);
+function jump() {
+    jumping = 1;
+    let jumpcout = 0;
+}
